317a6420f202528f2e0063b1e548729d26e7243e
lakrancoin@mail.ru <lakrancoin@mail.ru>
2019-04-10 20:16:31 +0300

    Signed-off-by: lakrancoin@mail.ru <lakrancoin@mail.ru>

--- src/rpccalls.cpp
+++ src/rpccalls.cpp
@@ -2,30 +2,97 @@
 // Created by mwo on 13/04/16.
 //
 
-#include "rpccalls.h"
+#include "RPCCalls.h"
 
 namespace xmreg
 {
 
-rpccalls::rpccalls(
-        string _deamon_url,
-        uint64_t _timeout)
-    : deamon_url {_deamon_url},
-      timeout_time {_timeout}
+RPCCalls::RPCCalls(string _deamon_url, uint64_t _timeout)
+    : deamon_url {_deamon_url}, timeout_time {_timeout}
 {
-epee::net_utils::parse_url(deamon_url, url);
+    epee::net_utils::parse_url(deamon_url, url);
 
-port = std::to_string(url.port);
+    port = std::to_string(url.port);
 
-timeout_time_ms = std::chrono::milliseconds {timeout_time};
+    timeout_time_ms = std::chrono::milliseconds {timeout_time};
 
-m_http_client.set_server(
-        deamon_url,
-        boost::optional<epee::net_utils::http::login>{});
+    m_http_client.set_server(
+            deamon_url,
+            boost::optional<epee::net_utils::http::login>{});
 }
 
+RPCCalls::RPCCalls(RPCCalls&& a)
+{
+    std::lock_guard<std::mutex> guard(a.m_daemon_rpc_mutex);
+
+    deamon_url = std::move(a.deamon_url);
+    timeout_time = a.timeout_time;
+    timeout_time_ms = a.timeout_time_ms;
+
+    url = std::move(a.url);
+    port = std::move(port);
+
+    // we can't move or copy m_http_client,
+    // so we just initialize it from zero
+    m_http_client.set_server(
+            deamon_url,
+            boost::optional<epee::net_utils::http::login>{});
+
+    // after the move, disconned the a object
+    a.m_http_client.disconnect();
+
+    cout << "\n RPCCalls(RPCCalls&& a) " << endl;
+}
+
+RPCCalls&
+RPCCalls::operator=(RPCCalls&& a)
+{
+    if (*this == a)
+        return *this;
+
+    std::unique_lock<std::mutex> lhs_lk(m_daemon_rpc_mutex, std::defer_lock);
+    std::unique_lock<std::mutex> rhs_lk(a.m_daemon_rpc_mutex, std::defer_lock);
+
+    std::lock(lhs_lk, rhs_lk);
+
+    deamon_url = std::move(a.deamon_url);
+    timeout_time = a.timeout_time;
+    timeout_time_ms = a.timeout_time_ms;
+
+    url = std::move(a.url);
+    port = std::move(port);
+
+    // we can't move or copy m_http_client,
+    // so we just initialize it from zero
+    m_http_client.set_server(
+            deamon_url,
+            boost::optional<epee::net_utils::http::login>{});
+
+    // after the move, disconned the a object
+    a.m_http_client.disconnect();
+
+    cout << "\n RPCCalls& operator=(RPCCalls&& a) " << endl;
+
+    return *this;
+}
+
+
 bool
-rpccalls::connect_to_monero_deamon()
+RPCCalls::operator==(RPCCalls const& a)
+{
+    return deamon_url == a.deamon_url;
+}
+
+
+bool
+RPCCalls::operator!=(RPCCalls const& a)
+{
+    return !(*this == a);
+}
+
+
+bool
+RPCCalls::connect_to_monero_deamon()
 {
     if(m_http_client.is_connected())
     {
@@ -35,151 +102,10 @@ rpccalls::connect_to_monero_deamon()
     return m_http_client.connect(timeout_time_ms);
 }
 
-uint64_t
-rpccalls::get_current_height()
-{
-    COMMAND_RPC_GET_HEIGHT::request   req;
-    COMMAND_RPC_GET_HEIGHT::response  res;
-
-    std::lock_guard<std::mutex> guard(m_daemon_rpc_mutex);
-
-    if (!connect_to_monero_deamon())
-    {
-        cerr << "get_current_height: not connected to deamon" << endl;
-        return false;
-    }
-
-    bool r = epee::net_utils::invoke_http_json(
-            "/getheight",
-            req, res, m_http_client, timeout_time_ms);
-
-    if (!r)
-    {
-        cerr << "Error connecting to Monero deamon at "
-             << deamon_url << endl;
-        return 0;
-    }
-    else
-    {
-        cout << "rpc call /getheight OK: " << endl;
-    }
-
-    return res.height;
-}
-
-bool
-rpccalls::get_mempool(vector<tx_info>& mempool_txs)
-{
-
-    COMMAND_RPC_GET_TRANSACTION_POOL::request  req;
-    COMMAND_RPC_GET_TRANSACTION_POOL::response res;
-
-    std::lock_guard<std::mutex> guard(m_daemon_rpc_mutex);
-
-    if (!connect_to_monero_deamon())
-    {
-        cerr << "get_current_height: not connected to deamon" << endl;
-        return false;
-    }
-
-    bool r = epee::net_utils::invoke_http_json(
-            "/get_transaction_pool",
-            req, res, m_http_client, timeout_time_ms);
-
-    if (!r)
-    {
-        cerr << "Error connecting to Monero deamon at "
-             << deamon_url << endl;
-        return false;
-    }
-
-
-    mempool_txs = res.transactions;
-
-    return true;
-}
 
 
 bool
-rpccalls::get_random_outs_for_amounts(
-        const vector<uint64_t>& amounts,
-        const uint64_t& outs_count,
-        vector<COMMAND_RPC_GET_RANDOM_OUTPUTS_FOR_AMOUNTS::outs_for_amount>& found_outputs,
-        string& error_msg)
-{
-    COMMAND_RPC_GET_RANDOM_OUTPUTS_FOR_AMOUNTS::request  req;
-    COMMAND_RPC_GET_RANDOM_OUTPUTS_FOR_AMOUNTS::response res;
-
-    req.outs_count = outs_count;
-    req.amounts = amounts;
-
-    std::lock_guard<std::mutex> guard(m_daemon_rpc_mutex);
-
-    if (!connect_to_monero_deamon())
-    {
-        cerr << "get_current_height: not connected to deamon" << endl;
-        return false;
-    }
-
-    bool r = epee::net_utils::invoke_http_bin(
-            "/getrandom_outs.bin",
-            req, res, m_http_client, timeout_time_ms);
-
-
-    if (!r || res.status == "Failed")
-    {
-        cerr << "rpc call to /getrandom_outs.bin failed for some reason" << endl;
-        return false;
-    }
-
-    if (!res.outs.empty())
-    {
-        found_outputs = res.outs;
-        return true;
-    }
-
-    return false;
-
-}
-
-/*
- * Not finished. get_random_outs_for_amounts is used instead of this.
- */
-bool
-rpccalls::get_out(
-        const uint64_t amount,
-        const uint64_t global_output_index,
-        COMMAND_RPC_GET_OUTPUTS_BIN::outkey& output_key)
-{
-
-
-    // generate output indices to request
-    COMMAND_RPC_GET_OUTPUTS_BIN::request req = AUTO_VAL_INIT(req);
-    COMMAND_RPC_GET_OUTPUTS_BIN::response res = AUTO_VAL_INIT(res);
-
-    req.outputs.push_back(get_outputs_out {amount, global_output_index});
-
-    bool r = epee::net_utils::invoke_http_bin("/get_outs.bin",
-                                              req, res, m_http_client,
-                                              timeout_time_ms);
-
-    if (!r || res.status == "Failed")
-    {
-        //error_msg = res.reason;
-
-        cerr << "Error /get_outs.bin: " << res.status << endl;
-        return false;
-    }
-
-    output_key = res.outs.at(0);
-
-    return true;
-
-}
-
-
-bool
-rpccalls::commit_tx(
+RPCCalls::commit_tx(
         const string& tx_blob,
         string& error_msg,
         bool do_not_relay)
@@ -234,53 +160,7 @@ rpccalls::commit_tx(
 }
 
 bool
-rpccalls::get_dynamic_per_kb_fee_estimate(
-        uint64_t grace_blocks,
-        uint64_t& fee,
-        string& error_msg)
-{
-    epee::json_rpc::request<COMMAND_RPC_GET_PER_KB_FEE_ESTIMATE::request>
-            req_t = AUTO_VAL_INIT(req_t);
-    epee::json_rpc::response<COMMAND_RPC_GET_PER_KB_FEE_ESTIMATE::response, std::string>
-            resp_t = AUTO_VAL_INIT(resp_t);
-
-
-    req_t.jsonrpc = "2.0";
-    req_t.id = epee::serialization::storage_entry(0);
-    req_t.method = "get_fee_estimate";
-    req_t.params.grace_blocks = grace_blocks;
-
-    std::lock_guard<std::mutex> guard(m_daemon_rpc_mutex);
-
-    if (!connect_to_monero_deamon())
-    {
-        cerr << "get_current_height: not connected to deamon" << endl;
-        return false;
-    }
-
-    bool r = epee::net_utils::invoke_http_json("/json_rpc",
-                                               req_t, resp_t,
-                                               m_http_client);
-
-
-    if (!r || resp_t.result.status == "Failed")
-    {
-        error_msg = resp_t.error;
-
-        cerr << "Error get_dynamic_per_kb_fee_estimate: " << error_msg << endl;
-
-        return false;
-    }
-
-    fee = resp_t.result.fee;
-
-    return true;
-
-}
-
-
-bool
-rpccalls::commit_tx(
+RPCCalls::commit_tx(
         tools::wallet2::pending_tx& ptx,
         string& error_msg)
 {
--- src/rpccalls.h
+++ src/rpccalls.h
@@ -18,9 +18,9 @@ using namespace crypto;
 using namespace std;
 
 
-class rpccalls
+class RPCCalls
 {
-    string deamon_url ;
+    string deamon_url;
     uint64_t timeout_time;
 
     std::chrono::milliseconds timeout_time_ms;
@@ -28,51 +28,41 @@ class rpccalls
     epee::net_utils::http::url_content url;
 
     epee::net_utils::http::http_simple_client m_http_client;
+
     std::mutex m_daemon_rpc_mutex;
 
     string port;
 
 public:
 
-    rpccalls(string _deamon_url = "http:://127.0.0.1:18081",
-             uint64_t _timeout = 200000);
+    RPCCalls(string _deamon_url = "http:://127.0.0.1:18081",
+             uint64_t _timeout = 200000);    
 
-    bool
+    RPCCalls(RPCCalls&& a);
+
+    RPCCalls&
+    operator=(RPCCalls&& a);
+
+    virtual bool
+    operator==(RPCCalls const& a);
+
+    virtual bool
+    operator!=(RPCCalls const& a);
+
+    virtual bool
     connect_to_monero_deamon();
-    uint64_t
-    get_current_height();
 
-    bool
-    get_mempool(vector<tx_info>& mempool_txs);
-
-    bool
-    get_random_outs_for_amounts(
-            const vector<uint64_t>& amounts,
-            const uint64_t& outs_count,
-            vector<COMMAND_RPC_GET_RANDOM_OUTPUTS_FOR_AMOUNTS::outs_for_amount>& found_outputs,
-            string& error_msg);
-    /*
-     * Not finished. get_random_outs_for_amounts is used instead of this.
-     */
-    bool
-    get_out(const uint64_t amount,
-            const uint64_t global_output_index,
-            COMMAND_RPC_GET_OUTPUTS_BIN::outkey& output_key);
-
-    bool
+    virtual bool
     commit_tx(const string& tx_blob,
               string& error_msg,
               bool do_not_relay = false);
 
-    bool
-    get_dynamic_per_kb_fee_estimate(
-            uint64_t grace_blocks,
-            uint64_t& fee,
-            string& error_msg);
-
-    bool
+    virtual bool
     commit_tx(tools::wallet2::pending_tx& ptx,
               string& error_msg);
+
+
+    virtual ~RPCCalls() = default;
 };
 
 
